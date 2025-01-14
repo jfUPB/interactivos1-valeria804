4c18ef44f7dede845696a7c097798d0975412438
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2025-01-13 20:10:33 -0500

    Actualizar texto en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Curso',
+			title: 'Sistemas Físicos Interactivos 1',
 			defaultLocale: 'root',
 			locales: {
 			  root: { label: 'Español', lang: 'es-ES' },
